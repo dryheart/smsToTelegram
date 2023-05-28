6d89e3cd65d74d0b94f7d70884f0f82ca7b09ab8
kdrysun <dryheart@gmail.com>
2023-05-28 10:03:07 +0900

    manifest 수정 telegram 발송 Timer 로 변경

--- app/src/main/java/com/kdrysun/smstotelegram/receiver/SmsReceiver.java
+++ app/src/main/java/com/kdrysun/smstotelegram/receiver/SmsReceiver.java
@@ -11,9 +11,7 @@ import com.kdrysun.smstotelegram.parser.SmsParser;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class SmsReceiver extends BroadcastReceiver {
 
@@ -21,34 +19,48 @@ public class SmsReceiver extends BroadcastReceiver {
 
     private final String TAG = "SMS_Receiver";
 
+    public static Timer timer;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(SMS_RECEIVED)) {
             Log.d(TAG, "onReceiver() 호출");
 
-            SmsMessage[] messagesFromIntent = Telephony.Sms.Intents.getMessagesFromIntent(intent);
-
-            String phoneNumber = "";
-            String msg = "";
-            List<Sms> smsList = new ArrayList<>();
-
-            for (SmsMessage message : messagesFromIntent) {
-                if (StringUtils.isNotBlank(phoneNumber) &&
-                        !StringUtils.equals(phoneNumber, message.getOriginatingAddress())) {
-
-                    smsList.add(new Sms(phoneNumber, msg,
-                            DateFormatUtils.format(new Date(), "yyyyMMddhhmmss")));
-                    msg = "";
-                }
-
-                phoneNumber = message.getOriginatingAddress();
-                msg += message.getMessageBody();
+            if (timer != null) {
+                timer.cancel();
+                timer = null;
             }
+            timer = new Timer();
 
-            smsList.add(new Sms(phoneNumber, StringUtils.trimToEmpty(msg),
-                    DateFormatUtils.format(new Date(), "yyyyMMddhhmmss")));
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    SmsMessage[] messagesFromIntent = Telephony.Sms.Intents.getMessagesFromIntent(intent);
 
-            new Thread(() -> new SmsParser().parse(context, smsList)).start();
+                    String phoneNumber = "";
+                    String msg = "";
+                    List<Sms> smsList = new ArrayList<>();
+
+                    for (SmsMessage message : messagesFromIntent) {
+                        if (StringUtils.isNotBlank(phoneNumber) &&
+                                !StringUtils.equals(phoneNumber, message.getOriginatingAddress())) {
+
+                            smsList.add(new Sms(phoneNumber, msg,
+                                    DateFormatUtils.format(new Date(), "yyyyMMddhhmmss")));
+                            msg = "";
+                        }
+
+                        phoneNumber = message.getOriginatingAddress();
+                        msg += message.getMessageBody();
+                    }
+
+                    smsList.add(new Sms(phoneNumber, StringUtils.trimToEmpty(msg),
+                            DateFormatUtils.format(new Date(), "yyyyMMddhhmmss")));
+
+                    new SmsParser().parse(context, smsList);
+                    //new Thread(() -> ).start();
+                }
+            }, 3000L);
         }
     }
 }
